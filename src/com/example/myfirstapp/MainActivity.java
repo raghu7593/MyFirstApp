05ddd08640cbdf685ec38e783addc5a6763bbfe6
Raghu Teja <raghutejaparisa@gmail.com>
2013-07-07 00:07:41 +0530

    Convert Fahrenheit to Celsius and Viceversa

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	private EditText text;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        text = (EditText) findViewById(R.id.editText1);
+      }
 
 
     @Override
@@ -23,13 +25,41 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    public void onClick(View view) {
+    	switch (view.getId()) {
+    		case R.id.button1:
+    			RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+    			RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+    			if (text.getText().length() == 0) {
+    				Toast.makeText(this, "Please enter a valid number",
+    						Toast.LENGTH_LONG).show();
+    				return;
+    			}
+    			float inputValue = Float.parseFloat(text.getText().toString());
+    			if (celsiusButton.isChecked()) {
+    				text.setText(String
+    						.valueOf(convertFahrenheitToCelsius(inputValue)));
+    				celsiusButton.setChecked(false);
+    				fahrenheitButton.setChecked(true);
+    			}
+    			else {
+    				text.setText(String
+    		                .valueOf(convertCelsiusToFahrenheit(inputValue)));
+    				fahrenheitButton.setChecked(false);
+    	            celsiusButton.setChecked(true);
+    			}
+    			break;
+    	}
+    }
     
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    // Converts to celsius
+    private float convertFahrenheitToCelsius(float fahrenheit) {
+    	return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // Converts to fahrenheit
+    private float convertCelsiusToFahrenheit(float celsius) {
+    	return ((celsius * 9) / 5) + 32;
     }
 }
